1e67b698f43e85109014478f5f8d2038e59a493c
Aaron Russo <arusso@berkeley.edu>
2014-01-27 11:21:41 -0800

    adding license and readme

---
+++ README.markdown
@@ -0,0 +1,37 @@
+# ClamAV Puppet Module #
+
+This module manages clamav by installing required software, and setting up
+scan jobs that can be run via cron or manually on the target host.
+
+Examples
+--------
+
+Setup a clamav scan that scans everything except the /sys and /dev, and does
+not run on a schedule.
+
+     clamav::scan { 'all-but-sys-and-dev':
+       exclude_dir => '(?i)/^\/(sys|dev)/',
+       scan        => '/',
+       enable      => false,
+     }
+
+License
+-------
+
+See LICENSE file
+
+Copyright
+---------
+
+Copyright &copy; 2014 The Regents of the University of California
+
+Contact
+-------
+
+Aaron Russo <arusso@berkeley.edu>
+
+Support
+-------
+
+Please log tickets and issues at the
+[Projects site](https://github.com/arusso/puppet-clamav/issues/)
--- manifests/scan.pp
+++ manifests/scan.pp
@@ -1,11 +1,71 @@
-# Class: clamav::scan
+# == Class: clamav::scan
 #
 # Setup a scan
 #
+# === Parameter:
+#
+# [*action_error*]
+#   Shell command to run if an error occurs during scanning. Does nothing by
+#   default.
+#
+# [*action_ok*]
+#   Shell command to execute if a scan returns ok. Does nothing by default.
+#
+# [*action_virus*]
+#   Shell command to run if a virus is detected. Does nothing by default.
+#
+# [*copy*]
+#   Copy infected files into DIRECTORY. Directory must be writable
+#   for the 'clam' user or unprivileged user running clamscan.
+#
+# [*enable*]
+#   Should this scan be enabled? Defaults to true.
+#
+# [*exclude*]
+#   Regular expression that, when matched, excludes a file from being scanned.
+#
+# [*exclude_dir*]
+#   Regular expression that, when matched, excludes a directory from being
+#   scanned.
+#
+# [*hour*]
+#   Hour (in cron format) to run the scan. Defaults to a consistently random
+#   hour based on the fqdn of the host. Has no impact if enable=false.
+#
+# [*include*]
+#   Regular expression that, when matched, will include a file to be scanned.
+#
+# [*include_dir*]
+#   Regular expression that, when matched, will include a directory to be
+#   scanned.
+
+# [*minute*]
+#   Minute (in cron format) to run the scan. Defaults to a consistently random
+#   minute based on the fqdn of the host. Has no impact if enable=false.
+#
+# [*month*]
+#   Month (in cron format) to run the scan. Runs every month by default.
+#
+# [*monthday*]
+#   Month day (in cron format) to run the scan. Runs every month day by
+#   default.
+#
+# [*move*]
+#   Move infected files into DIRECTORY. Directory must be writable
+#   for the 'clam' user or unprivileged user running clamscan.
+#
+# [*quiet*]
+#   Be quiet (only print error messages).
+#
+# [*recursive*]
+# [*scan*]
+# [*scanlog*]
+# [*weekday*]
+#
 define clamav::scan (
-  $action_virus = '',
   $action_error = '',
   $action_ok = '',
+  $action_virus = '',
   $enable = true,
   $hour = fqdn_rand(23,$title),
   $minute = fqdn_rand(59,$title),
@@ -17,8 +77,8 @@ define clamav::scan (
   $flags = '',
   $include = [ ],
   $include_dir = [ ],
-  $quiet = true,
   $move = false,
+  $quiet = true,
   $recursive = false,
   $scan = [ ],
   $scanlog = "/var/log/clamav/scan_${title}",
